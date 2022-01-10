09a77d6fb1b46ab34a8e329a20cc86d3ef108136
ada024 <andreasm.dale@bkkfiber.no>
2022-01-10 21:09:23 +0100

    Updated menu-items to naviagate withRouter Higher order Component(HOC)

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,9 +49,10 @@ class Directory extends React.Component {
     render() {
        return (
            <div className='directory-menu'>  {/* Destructuring, pulling out properties from sections-object */}
-               {this.state.sections.map(({ title, imageUrl, id, size}) => (
-                       <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-                   ))}
+               {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                   <MenuItem key={id} {...otherSectionProps} />
+               ))}
+               {/* title={title} imageUrl={imageUrl} size={size} = same as ...otherSectionProps */}
            </div>
        );
     } //render
