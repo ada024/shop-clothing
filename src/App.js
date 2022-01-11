2cfc4cdfdfcc5032bc3f3c648581b7c1abf262c3
ada024 <andreasm.dale@bkkfiber.no>
2022-01-11 18:00:11 +0100

    Added header (navbar)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 // function-component
 const HatsPage = () => (
@@ -15,10 +16,11 @@ const HatsPage = () => (
 
 function App() {
     return (
-        <div className="App">
+        <div>
+            <Header/>
             <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
             </Switch>
         </div>
     );
