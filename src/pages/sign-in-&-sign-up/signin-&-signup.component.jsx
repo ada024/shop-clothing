96dc730310b2b22c5a0da6cb444bff33b5ed9276
ada024 <andreasm.dale@bkkfiber.no>
2022-01-13 10:11:51 +0100

    Added ability to store authenticated user

--- src/pages/sign-in-&-sign-up/signin-&-signup.component.jsx
+++ src/pages/sign-in-&-sign-up/signin-&-signup.component.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import './signin-&-signup.styles.scss';
 import SignIn from "../../components/signin/signin.component";
+import SignUp from "../../components/sign-up/signup.compoonent";
 
 
 const SigninSignupPage = () => (
-    <div className='signin-&-signup'>
+    <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp/>
     </div>
-
-
 );
 
 export default SigninSignupPage;
