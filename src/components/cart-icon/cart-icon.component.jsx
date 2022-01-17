c73cdaab1270813e5ed85474f9d077123407893e
ada024 <andreasm.dale@bkkfiber.no>
2022-01-17 13:33:12 +0100

    Updated our app with reselect, replaced cart selector with memoized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {connect} from "react-redux";
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 
 
 import './cart-icon.styles.scss';
+import {selectCartItemsCount} from "../../redux/cart/cart.selector";
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -19,5 +20,16 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-// null is the defailt when no state.
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => {
+    // console.log("ItemCount computed");
+    return ({
+        itemCount: selectCartItemsCount(state)
+    })
+};
+
+
+// null is the default when no state.
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CartIcon);
