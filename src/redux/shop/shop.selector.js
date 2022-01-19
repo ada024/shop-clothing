280efb1f82be5e147c58e6bed194ba6d16329826
ada024 <andreasm.dale@bkkfiber.no>
2022-01-19 12:31:43 +0100

    Created respective selectors & updated corresponding components with redux flow for directory/shop

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,31 @@
+import {createSelector} from "reselect";
+import memoize from 'lodash.memoize';
+
+
+const selectShop = state => state.shop;
+
+/*
+export const selectCollections = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        (collections) => collections[collectionUrlParam]
+    )
+);
+*/
+
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+    createSelector(
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    );
